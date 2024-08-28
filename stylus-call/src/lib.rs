3a52b8184b577d2ea23fb2535356c0bd5fac0729
Daniel Bigos <daniel.bigos@icloud.com>
2024-08-28 11:12:33 +0200

    feat: add simple example of calling external contract from Stylus

--- stylus-call/src/lib.rs
+++ stylus-call/src/lib.rs
@@ -4,24 +4,64 @@ extern crate alloc;
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
+use alloy_primitives::{Address, FixedBytes};
 use alloy_sol_types::sol;
-use stylus_sdk::prelude::{entrypoint, external, sol_storage};
+use stylus_sdk::{
+    abi::Bytes,
+    call::Call,
+    console,
+    prelude::{entrypoint, external, sol_interface, sol_storage, SolidityError},
+};
 
 sol! {
     #[allow(missing_docs)]
     event CallBytes(bytes data);
 }
 
+sol! {
+    #[derive(Debug)]
+    #[allow(missing_docs)]
+    error ReceiverError();
+}
+
+#[derive(SolidityError, Debug)]
+pub enum Error {
+    RecError(ReceiverError),
+}
+
+sol_interface! {
+    interface SolidityReceiver {
+        #[allow(missing_docs)]
+        function handle(
+            bytes calldata data
+        ) external returns (bytes4);
+    }
+}
+
 sol_storage! {
     #[entrypoint]
     pub struct CallPoC {
-        bool initialized;
+        uint8 value;
     }
 }
 
 #[external]
 impl CallPoC {
-    pub fn easy(&self) -> u8 {
-        11
+    pub fn dummy(&mut self, receiver: Address, data: Bytes) -> Result<FixedBytes<4>, Error> {
+        let receiver = SolidityReceiver::new(receiver);
+        let call = Call::new_in(self);
+        let data = data.to_vec().into();
+        console!("Calldata : {:x?}", data);
+        let result = receiver.handle(call, data);
+        match result {
+            Ok(retval) => {
+                console!("It works: {:?}", retval);
+                Ok(retval)
+            }
+            Err(e) => {
+                console!("Error: {:?}", e);
+                Err(Error::RecError(ReceiverError {}))
+            }
+        }
     }
 }
